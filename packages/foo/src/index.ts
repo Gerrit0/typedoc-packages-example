b75dcd6ec1f63e3f8de4174d8586f8ad37a8069c
An Phi <aphi@skidmore.edu>
2023-04-08 19:35:23 -0400

    reproduce weird warning in td24

--- packages/foo/src/index.ts
+++ packages/foo/src/index.ts
@@ -4,6 +4,21 @@
  */
 
 import { bar } from "@typedoc/bar";
+import {
+  cloneDeep as deepClone,
+  isEqual as deepEqual,
+  findLast,
+  isEmpty,
+  pickBy,
+  uniqBy,
+  uniq,
+  debounce,
+  throttle,
+  mergeWith,
+  toNumber,
+  type DebouncedFunc,
+  isObject,
+} from "lodash-es";
 
 /**
  * Docs for `foo` function, which returns a {@link @typedoc/bar!BarInt}
@@ -11,3 +26,18 @@ import { bar } from "@typedoc/bar";
 export function foo() {
   return bar();
 }
+
+// NOTE: We re-export lodash utilities like this so we centralize utility usage in our app
+// in case we want to swap out the implementation
+export {
+  deepClone,
+  deepEqual,
+  findLast,
+  isEmpty,
+  pickBy,
+  uniqBy,
+  uniq,
+  debounce,
+  throttle,
+  type DebouncedFunc,
+};
